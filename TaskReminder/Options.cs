4343fbae7a2cd1d63006fe75a3cf8e7ea4a553bf
YevSoft <str.lucky86@gmail.com>
2017-02-11 13:12:06 +0100

    added supporting of command line arguments,

---
+++ TaskReminder/Options.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace TaskReminder
+{
+    public class Options
+    {
+        [Option('h', "html", Required = true, HelpText = "Input html template file to be processed.")]
+        public string HtmlTemplate { get; set; }
+
+        [Option('d', "task", Required = true, HelpText = "Input tasks file to be processed.")]
+        public string Task { get; set; }
+
+        [Option('t', "timeout", Required = true, HelpText = "Timeout for displaying dashboard before closing.")]
+        public string Timeout { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
--- TaskReminder/Program.cs
+++ TaskReminder/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TaskReminder
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard());
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Dashboard(options));
+            }
         }
     }
 }
